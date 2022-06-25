d0fcb4b6d228c39007510fa3d2e067ecef7cd450
Md.Samiul Haque <samiulh4@gmail.com>
2022-06-26 01:32:04 +0600

    stack implementation

--- Python/practices/practice_0001.py
+++ Python/practices/practice_0001.py
@@ -12,6 +12,7 @@ def create_stack():
 def push_stack(stack, item):
     stack.append(item)
     print("pushed item: " + item)
+    return len(stack) - 1
 
 
 # Checking an empty stack
@@ -23,13 +24,26 @@ def check_empty(stack):
         return True
 
 
+def print_stack (stack):
+    print('Printing Stack...')
+    # start point, stop point, increment/decrement
+    for i in range(len(stack)-1, -1, -1):     
+        print(stack[i])     
+# end -:- print_stack()           
+
+
 
 
 if __name__ == '__main__':
     print('Starting Program...')
+    top = -1
     stack = create_stack()
     # print(check_empty(stack))
-    push_stack(stack, str(9))
-    push_stack(stack, str(5))
-    push_stack(stack, str(3))
-    push_stack(stack, str(8))
+    top = push_stack(stack, str(9))
+    top = push_stack(stack, str(5))
+    top = push_stack(stack, str(7))
+    top = push_stack(stack, str(1))
+    top = push_stack(stack, str(3))
+    top = push_stack(stack, str(8))
+    print(top)
+    #print_stack(stack)
